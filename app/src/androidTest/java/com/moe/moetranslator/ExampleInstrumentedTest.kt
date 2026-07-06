223118b2eb7147358ce6cab1f5844161c1c3ae7b
User <user@example.com>
2026-07-06 17:26:17 +0800

    chore: bump version to 0.8.5, change applicationId to com.moe.starflow

--- app/src/androidTest/java/com/moe/moetranslator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/moe/moetranslator/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.moe.moetranslator", appContext.packageName)
+        assertEquals("com.moe.starflow", appContext.packageName)
     }
 }
