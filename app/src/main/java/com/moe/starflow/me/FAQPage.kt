6200b096f8e3a64177f9fc9e16c1e3401b9c1eee
User <user@example.com>
2026-07-26 23:19:33 +0800

    feat: 模型管理页添加 FAQ 路径指引 + 新增手动下载 FAQ + manga-ocr 标注识别速度较慢

--- app/src/main/java/com/moe/starflow/me/FAQPage.kt
+++ app/src/main/java/com/moe/starflow/me/FAQPage.kt
@@ -51,6 +51,7 @@ class FAQPage : Fragment() {
             CustomCard(getText(R.string.faq_q6), getText(R.string.faq_a6)),
             CustomCard(getText(R.string.faq_q7), getText(R.string.faq_a7)),
             CustomCard(getText(R.string.faq_q8), getText(R.string.faq_a8)),
+            CustomCard(getText(R.string.faq_q9), getText(R.string.faq_a9)),
             CustomCard(getText(R.string.faq_q10), getText(R.string.faq_a10)),
             CustomCard(getText(R.string.faq_q11), getText(R.string.faq_a11))
         )
